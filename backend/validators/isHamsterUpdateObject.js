78ea11c286684a2980777570d9cf89a16f120f04
sayttplx <you@example.com>
2021-10-27 12:39:22 +0200

    minor fix on validator

--- backend/validators/isHamsterUpdateObject.js
+++ backend/validators/isHamsterUpdateObject.js
@@ -5,12 +5,12 @@ const isHamsterUpdateObject = (obj) => {
     const keys = Object.keys(obj);
     const value = Object.values(obj);
 
-    if (!keys.includes('wins') || !keys.includes('games')) {
+    if (!keys.includes('wins') && !keys.includes('games') && !keys.includes('defeats')) {
         return false;
     }
 
     const filter = value.filter(x => (typeof x === 'number'));
-    return filter.length === 2;
+    return filter.length === 2 || 3 ;
 }
 
 module.exports = isHamsterUpdateObject;
