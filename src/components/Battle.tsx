6436fe462f2857c2dda6d1c0bbd0ad3443f7a4a1
sayttplx <you@example.com>
2021-11-01 15:33:52 +0100

    added button to battle

--- src/components/Battle.tsx
+++ src/components/Battle.tsx
@@ -1,5 +1,25 @@
 import { useState, useEffect } from 'react';
 import { Hamster } from '../models/Hamster'
+import { Grid } from './battle/Grid'
+import { BattleField } from './battle/Battlefield'
+import axios from 'axios'
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+    background-color: #ff5c5c;
+    border: none;
+    width: 100px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 5px;
+`;
 
 
 const Battle = () => {
@@ -10,21 +30,17 @@ const Battle = () => {
     const [voteButton, setVoteButton] = useState(true)
 
 
-
+    
     async function getRandomHamster() {
-        const resOne = await fetch('/hamsters/random')
-        const hamsterOne = await resOne.json()
-        setCombatantOne(hamsterOne)
-        const resTwo = await fetch('/hamsters/random')
-        const hamsterTwo = await resTwo.json()
-        setCombatantTwo(hamsterTwo)
+        const resOne = await axios.get('/hamsters/random')
+        const resTwo = await axios.get('/hamsters/random')
+        setCombatantOne(resOne.data)
+        setCombatantTwo(resTwo.data)
     }
-
 
     useEffect(() => {
         getRandomHamster()
     }, [])
-
 
     const next = () => {
         getRandomHamster()
@@ -33,167 +49,85 @@ const Battle = () => {
         setVoteButton(true)
     }
 
-
-    if (!combatantOne || !combatantTwo) {
-        return <div>
-            <h1>Loading...</h1>
-        </div>
-    }
-
-
+    if (!combatantOne || !combatantTwo) { return <div>Loading...</div> }
+        
     async function voteOne() {
 
-        if (!combatantOne || !combatantTwo) {
-            return
-        }
-
+        if (!combatantOne || !combatantTwo) { return }
+            
         const winnerId = combatantOne.id
         const loserId = combatantTwo.id
 
-
-        setCombatantOne({
-            ...combatantOne,
-            wins: combatantOne.wins + 1,
-            games: combatantOne.games + 1
-        })
-
-
-        setCombatantTwo({
-            ...combatantTwo,
-            games: combatantTwo.games + 1,
-            defeats: combatantTwo.defeats + 1
-        })
-
-
-        const changeWinner = {
-            wins: combatantOne.wins + 1,
-            games: combatantOne.games + 1
-        }
-
-        const changeLoser = {
-            games: combatantTwo.games + 1,
-            defeats: combatantTwo.defeats + 1
-        }
+        setCombatantOne({ ...combatantOne, wins: combatantOne.wins + 1, games: combatantOne.games + 1 })
+        setCombatantTwo({ ...combatantTwo, games: combatantTwo.games + 1, defeats: combatantTwo.defeats + 1 })
+            
+        const changeWinner = { wins: combatantOne.wins + 1, games: combatantOne.games + 1 }
+        const changeLoser = { games: combatantTwo.games + 1, defeats: combatantTwo.defeats + 1 }
 
         setCombatantStatsOne(true)
         setVoteButton(false)
 
-        await fetch(`/hamsters/${winnerId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(changeWinner)
-        })
-
-        await fetch(`/hamsters/${loserId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(changeLoser)
-        })
+        await axios.put(`/hamsters/${winnerId}`, changeWinner)
+        await axios.put(`/hamsters/${loserId}`, changeLoser)
 
     }
 
     async function voteTwo() {
 
-        if (!combatantOne || !combatantTwo) {
-            return
-        }
+        if (!combatantOne || !combatantTwo) { return }
 
         const winnerId = combatantTwo.id
         const loserId = combatantOne.id
 
-        setCombatantTwo({
-            ...combatantTwo,
-            wins: combatantTwo.wins + 1,
-            games: combatantTwo.games + 1
-        })
+        setCombatantTwo({ ...combatantTwo, wins: combatantTwo.wins + 1, games: combatantTwo.games + 1 })
+        setCombatantOne({ ...combatantOne, games: combatantOne.games + 1, defeats: combatantOne.defeats + 1 })
 
-        setCombatantOne({
-            ...combatantOne,
-            games: combatantOne.games + 1,
-            defeats: combatantOne.defeats + 1
-        })
-
-        const changeWinner = {
-            wins: combatantTwo.wins + 1,
-            games: combatantTwo.games + 1
-        }
-
-        const changeLoser = {
-            games: combatantOne.games + 1,
-            defeats: combatantOne.defeats + 1
-        }
+        const changeWinner = { wins: combatantTwo.wins + 1, games: combatantTwo.games + 1 }
+        const changeLoser = { games: combatantOne.games + 1, defeats: combatantOne.defeats + 1 }
 
         setCombatantStatsTwo(true)
         setVoteButton(false)
 
-
-        await fetch(`/hamsters/${winnerId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(changeWinner)
-        })
-
-        await fetch(`/hamsters/${loserId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(changeLoser)
-        })
+        await axios.put(`/hamsters/${winnerId}`, changeWinner)
+        await axios.put(`/hamsters/${loserId}`, changeLoser)
     }
 
     return (
-        <>
+        <BattleField>
+
             <h1>Battle</h1>
-            <img src={`/img/${combatantOne.imgName}`} alt="hamster" height="300px" width="300px" />
 
-            <h2>{combatantOne.name}</h2>
-            {voteButton && <button onClick={voteOne}>Vote</button>}
+            <Grid>
 
-            <br />
-            <br />
-            {(combatantStatsOne || combatantStatsTwo) &&
-                <div>
-                    <p>Wins: {combatantOne.wins}</p>
-                    <p>Games: {combatantOne.games}</p>
-                    <p>Defeats: {combatantOne.defeats}</p>
-                    <br />
+                <div className="grid-item">
+                    <img src={`/img/${combatantOne.imgName}`} alt={combatantOne.name} />
+                    <h1>{combatantOne.name}</h1>
+                    {voteButton && <button onClick={voteOne}>Vote</button>}
+                    {(combatantStatsOne || combatantStatsTwo) &&
+                        <div className="stats">
+                            <h3>Wins: {combatantOne.wins}</h3>
+                            <h3>Games: {combatantOne.games}</h3>
+                            <h3>Defeats: {combatantOne.defeats}</h3>
+                        </div>}
                 </div>
-            }
 
-
-            vs
-            <br />
-            <br />
-            <img src={`/img/${combatantTwo.imgName}`} alt="hamster" height="300px" width="300px" />
-
-            <h2>{combatantTwo.name}</h2>
-            {voteButton && <button onClick={voteTwo}>Vote</button>}
-            <br />
-            <br />
-            {(combatantStatsOne || combatantStatsTwo) &&
-                <div>
-                    <p>Wins: {combatantTwo.wins}</p>
-                    <p>Games: {combatantTwo.games}</p>
-                    <p>Defeats: {combatantTwo.defeats}</p>
-                    <br />
+                <div className="grid-item">
+                    <img src={`/img/${combatantTwo.imgName}`} alt={combatantTwo.name} />
+                    <h1>{combatantTwo.name}</h1>
+                    {voteButton && <button onClick={voteTwo}>Vote</button>}
+                    {(combatantStatsOne || combatantStatsTwo) &&
+                        <div className="stats">
+                            <h3>Wins: {combatantTwo.wins}</h3>
+                            <h3>Games: {combatantTwo.games}</h3>
+                            <h3>Defeats: {combatantTwo.defeats}</h3>
+                        </div>}
                 </div>
-            }
 
-            <button onClick={next}>Next</button>
+            </Grid>
 
-            <h1>{combatantOne.id}</h1>
-            <br />
-            <br />
-            <h1>{combatantTwo.id}</h1>
+            <Button onClick={next}>Next</Button>
 
-        </>
+        </BattleField>
     )
 };
 
